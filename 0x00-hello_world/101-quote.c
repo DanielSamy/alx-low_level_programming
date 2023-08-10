f8255c03e61d2ae765cbc3e38d6342a129cd45e2
DanielSamy <danielsamy2002@gmail.com>
2023-08-10 12:45:00 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar,
+	char *message = ("and that piece of art is useful\" - Dora Korpar,
 
-	     2015-10-19\n";
+	2015-10-19\n");
 
 	write(2, message, 59);
 	return (1);
