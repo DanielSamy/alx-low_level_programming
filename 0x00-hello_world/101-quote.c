03b655ff7ee1d9c7ec476c08e8d759c6d9e43106
DanielSamy <danielsamy2002@gmail.com>
2023-08-10 12:23:09 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This C program print a qoute with pointer and write function
+ *
+ * Return: 1 (not Success)
+ */
+
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 59);
+	return (1);
+}
