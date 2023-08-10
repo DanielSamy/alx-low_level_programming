b133aa94d13ab01edfb36025fc9412134cf315c1
DanielSamy <danielsamy2002@gmail.com>
2023-08-10 13:16:31 -0700

    00101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: This C program print a qoute with pointer and write function
+ * Description: This C program print a qoute with pointer and writ function
  *
  * Return: 1 (not Success)
  */
 int main(void)
 {
-	write(2,
-	 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 59);
 	return (1);
 }
