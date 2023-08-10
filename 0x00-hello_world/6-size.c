7f51b9691c5762fa4725ef57a9fcd5873c3d7c93
DanielSamy <danielsamy2002@gmail.com>
2023-08-10 10:54:48 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,9 @@ int main(void)
 	long int x;
 	long long d;
 	float f;
+
 	printf("Size of a char:%lu byte(s)\n", (unsigned long) sizeof(c));
-	printf("Size of a int:%u byte(s)\n",(unsigned long) sizeof(i));
+	printf("Size of a int:%u byte(s)\n", (unsigned long) sizeof(i));
 	printf("Size of a long int:%u byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of a long long int:%u byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float:%u byte(s)\n", (unsigned long)sizeof(f));
