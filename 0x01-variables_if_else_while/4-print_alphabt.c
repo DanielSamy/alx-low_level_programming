01db9ab87ef17541aee2b0f090d5d26c6c7f821d
DanielSamy <danielsamy2002@gmail.com>
2023-08-11 11:01:48 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This C program prints the alphabet letters except e and q
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(ch[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
