8504077ff42c4da3379f2942733f1410a258e716
DanielSamy <danielsamy2002@gmail.com>
2023-08-15 10:10:35 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+*Description: print _putchar
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char arr[] = "_putchar";
+	int i;
+
+	for (i = 0; i < arr; i++)
+		_putchar(arr[i]);
+	_putchar("\n");
+
+	return (0);
+}
