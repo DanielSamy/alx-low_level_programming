bcd08655e4beb91f81c8f98ed8e136371842183f
DanielSamy <danielsamy2002@gmail.com>
2023-08-15 15:19:53 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,19 +11,28 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar(48);
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			prod = num * mult;
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + 48);
+			if ( mult == 0)
+			{
+				_putchar(prod + '0');
+			}
 
-			_putchar((prod % 10) + 48);
+			if (prod < 10 && mult != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else if (prod >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
 		}
 	}	_putchar('\n');
 }
