298b404ee1813f14bf39aeba7a5b1c8c8e4f663f
DanielSamy <danielsamy2002@gmail.com>
2023-08-17 05:38:13 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; i <= 57; i++)
+		_putchar(c);
+	_putchar('\n');
+}
