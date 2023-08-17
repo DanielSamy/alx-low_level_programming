3ec8cde39fe880c3c91457b1900d8dde4bd18043
DanielSamy <danielsamy2002@gmail.com>
2023-08-17 10:22:06 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,13 +12,21 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 	else
+	{
 		for (i = 1; i <= size; i++)
 		{
 			for (j = 1; j <= size; j++)
+			{
 				_putchar(' ');
+			}
+
 			for (j = 1; j <= i; j++)
+			{
 				_putchar('#');
+			}
+
 			_putchar('\n');
 		}
+	}
 
 }
