c58f920ae111630fe203d6d27df99a5af1a8439f
DanielSamy <danielsamy2002@gmail.com>
2023-08-17 06:02:55 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 execpt 2 and 4
+ * Retuen: Always: 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	char num;
+
+	for (num >= '0'; num <= '9'; num++)
+	{
+		if (num == '2' || num == '4')
+			continue;
+			_putchar(num);
+	}
+	_putchar('\n');
+}
