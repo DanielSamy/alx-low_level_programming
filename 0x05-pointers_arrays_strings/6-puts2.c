8e89e45e74af8f24d470de4c5d7783e648b016ab
DanielSamy <danielsamy2002@gmail.com>
2023-08-21 12:26:34 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function print every character of sting
+ * @str: string parameter input
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		i++;
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
