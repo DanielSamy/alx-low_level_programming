b1b2432dcaf768ccb419e9b9208a8665fda50ed6
DanielSamy <danielsamy2002@gmail.com>
2023-08-28 11:22:03 -0700

    8print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,13 +9,12 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, s1, s2;
+	int i, s1 = 0, s2 = 0;
 
-	s1, s2 = 0;
 	for (i = 0; i < size; i++)
 	{
 		s1 += a[i];
-		s2 += a[size - itr - 1];
+		s2 += a[size - i - 1];
 		a += size;
 	}
 	printf("%d, ", s1);
