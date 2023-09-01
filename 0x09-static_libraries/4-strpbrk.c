67e1d8afdbc2013242ed46a481184e5ef81d9d33
DanielSamy <danielsamy2002@gmail.com>
2023-09-01 10:25:01 -0700

    static_libraries

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: always 0 (success)
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
