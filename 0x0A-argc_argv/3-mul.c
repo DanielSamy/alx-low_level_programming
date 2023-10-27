6d0507737717f7e361852157701a92c775ae669f
DanielSamy <danielsamy2002@gmail.com>
2023-10-27 12:29:28 -0700

    comm

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(aegv[1] * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+}
